bbfdd66da5a1b2efdac7518c0bec76ff4a7abbf0
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2025-07-02 22:44:21 -0400

    Create MarvelRivals-AutoCloseProcess-FirstRun.bat

---
+++ Mods/MarvelRivals/MarvelRivals-AutoCloseProcess/MarvelRivals-AutoCloseProcess-FirstRun.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "powershell.exe" -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Scripts\MarvelRivals-AutoCloseProcess.ps1"
+start "" "C:\Games\Marvel Rivals\MarvelRivals.exe"
